853970bd5a0093e7b2b007358765d0357e1f58ad
Enzo Brito Pavanelli <enzobpavanelli@gmail.com>
2026-06-02 17:38:18 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,14 @@
-const CACHE = 'eq30-v3';
+const CACHE = 'eq30-v4';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './icon.png',
-  'https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;400;500&family=Syne:wght@400;600;700&display=swap'
+  './icon.png'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {}))
+    caches.open(CACHE).then(c => c.addAll(ASSETS).catch((err) => console.log("Erro no cache:", err)))
   );
   self.skipWaiting();
 });
@@ -27,9 +26,8 @@ self.addEventListener('fetch', e => {
   if(e.request.method !== 'GET') return;
   e.respondWith(
     caches.match(e.request).then(cached => {
-      if(cached) return cached;
-      return fetch(e.request).then(res => {
-        if(res && res.status === 200 && res.type !== 'opaque'){
+      return cached || fetch(e.request).then(res => {
+        if(res && res.status === 200 && res.type === 'basic'){
           const clone = res.clone();
           caches.open(CACHE).then(c => c.put(e.request, clone));
         }
